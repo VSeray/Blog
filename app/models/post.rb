8ae93303ec8c51afff47967b29197c5a8c92890f
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-05-05 08:53:24 +0300

    Update files for validation models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
   has_many :comments
   has_many :likes
-  belongs_to :author, class_name: 'User'
+  belongs_to :user
 
   def recent_5_comments
     comments.order('created_at Desc').limit(5)
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,20 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/0/posts'
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template(:index)
-      expect(response.body).to include('Here is a page for showing list for post for a given user')
-    end
+    before(:each) { get user_posts_path user_id: 32 }
 
-    describe 'GET /show' do
-      it 'returns show page' do
-        get '/users/0/posts/show'
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:show)
-        expect(response.body).to include('Here is a page for showing a post in detail and its comments')
-      end
+    it 'returns http 200 success' do
+      expect(response).to have_http_status(200)
+    end
+    it 'should render the correct template' do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) { get user_post_path user_id: 32, id: 54 }
+
+    it 'should return correct response' do
+      expect(response).to have_http_status(200)
+    end
+    it 'should render the correct template' do
+      expect(response).to render_template(:show)
     end
   end
 end

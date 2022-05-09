cf755061627eeaaf3d27120c53d99a774f52d24f
tYohannes <teklegy@gmail.com>
2022-05-09 17:43:31 +0300

    Refactor code to replace user_id with author_id

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,14 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
+    @user = @post.author
     new_like = current_user.likes.new(
-      user_id: current_user.id,
+      author_id: current_user.id,
       post_id: @post.id
     )
     new_like.update_likes_counter
     if new_like.save
-      redirect_to "/users/#{@post.user_id}/posts/#{@post.id}", notice: 'Success!'
+      redirect_to user_post_path(@user, @post), notice: 'Success!'
     else
       redirect_to "/users/#{@post.user_id}/posts/#{@post.id}", alert: 'Error occured!'
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,22 +15,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_100726) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
-    t.bigint "user_id", null: false
-    t.string "text"
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -40,8 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_100726) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,8 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_09_100726) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end

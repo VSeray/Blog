4d6725443fe4bd26f9f31e4b62e3336b459329d5
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-05-10 14:24:09 +0300

    Add authorization resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  load_and_authorize_resource
-
+  skip_before_action :authenticate_user!, only: %i[index show]
+  
   def index
     @user = current_user
     @posts = @user.posts.includes(:comments)
